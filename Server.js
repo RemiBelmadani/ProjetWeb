1da5acb1254222fa62e20b0237de504e58a7f005
RemiBelmadani <94165667+RemiBelmadani@users.noreply.github.com>
2022-12-02 09:21:45 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,14 +1,27 @@
-const MySQL = require("MySQLWorkbench");
-const pool = MySQLWorkbench.createPool({
-    host: "localhost", user: "root", password: "123Layla", database: "ProjetL3"
+/*
+mkdir controllers models views
+npm init -y
+npm install express express-session body-parser passport dotenv ejs
+>> https://www.npmjs.com/package/body-parser
+edit/copy files, then ... node server.js 
+*/
+//console.log('wolololo');
+//return;
+/*
+console.log('wolololo');
+
+
+const mysql = require('mysql2/promise');
+const conn = mysql.createConnection({
+    host:'localhost', user: 'root', database: 'Jewelry', password: 'Ad0f9e83f10', debug: false
 });
-pool.getConnection().then(function(conn){
-    conn.query("SELECT * FROM Clients").then(function(rows){ console.log(rows) });
-    conn.query("SELECT * FROM Jewels").then(function(rows){ console.log(rows); process.exit(); });
-    conn.query("SELECT * FROM Orders").then(function(rows){ console.log(rows) });
+conn.then(function(conn) {
+    conn.execute('SELECT * FROM Jewels').then(function(result){ const [rows, fields]=result; console.log(rows); });
+    conn.execute('SELECT * FROM Orders').then(function(result){ const [rows, fields]=result; console.log(rows); });
+    conn.execute('SELECT * FROM Users').then(function(result){ const [rows, fields]=result; console.log(rows); process.exit(); });
 });
 return;
-
+*/
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -23,7 +36,7 @@ app.listen(process.env.WEB_PORT, '0.0.0.0',
 
 app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'!
     let clientIp = request.ip;
-    response.send(`Hello, dear ${clientIp}. I am a nodejs website...`);
+    response.send('Hello, dear ${clientIp}. I am a nodejs website...');
     response.end(); // optional
 });
 
@@ -36,15 +49,15 @@ const session = require("express-session");
 app.use(session({
     secret: "SecretRandomStringDskghadslkghdlkghdghaksdghdksh",
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day in msec
+    //cookie: { maxAge: 1000 60*60*24 }, // 1 day in msec
     resave: false
 }));
 
-const auth = require("./utils/users.auth");
+const auth = require("./utils/usersAuth");
 auth.initialization(app);
 
 // app.use(routeBase, callback);
 app.use("/static", express.static(__dirname + '/static'));
-app.use("/hello", require("./controllers/hello.route"));
-app.use("/cars", require("./controllers/cars.route"));
-app.use("/auth", require("./controllers/auth.route"));
+//app.use("/hello", require("./controllers/hello.route"));
+app.use("/Jewels", require("./controllers/jewelsControls"));
+app.use("/Users", require("./controllers/usersControllers"));
