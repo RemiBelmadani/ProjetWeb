413400c64238dc4e2f1550ce48f11defb2dfb369
RemiBelmadani <94165667+RemiBelmadani@users.noreply.github.com>
2022-12-02 09:20:14 +0100

    Update MySQL.js

--- MySQL.js
+++ MySQL.js
@@ -1,13 +1,10 @@
-MySQL.js
-// utils/db.js
-
-const MySQL = require("MySQL");
+const MySQL = require("mysql2/promise");
 
 const pool = MySQL.createPool({
-    host: process.env.localhost,
-    user: process.env.root,
-    password: process.env.123Layla,
-    database: process.env.ProjetL3
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = {
