bf8485de9803e81d23fb5cd5566f829402e6e06f
RemiBelmadani <94165667+RemiBelmadani@users.noreply.github.com>
2022-12-05 10:47:58 +0100

    Update jewelsControls.js

--- jewelsControls.js
+++ jewelsControls.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const JewelRepo = require('../utils/repositoryJewels.js');
+const JewelRepo = require('../utils/repositoryJewels');
 router.get('/', JewelRootAction);
 router.get('/list', JewelListAction);
 router.get('/show/:Jewel_ID', JewelShowAction);
-//router.get('/del/:Jewel_ID', JewelDelAction);
+router.get('/del/:Jewel_ID', JewelDelAction);
 router.get('/edit/:Jewel_ID', JewelEditAction);
 router.post('/update/:Jewel_ID', JewelUpdateAction);
 
@@ -14,35 +14,34 @@ function JewelRootAction(request, response) {
 }
 async function JewelListAction(request, response) {
     // response.send("LIST ACTION");
-    var Jewels = await JewelRepo.getAllJewels();
-    // console.log(cars);
+    var Jewels = await JewelRepo.getAllJewels(); 
+    //console.log(Jewels);
     var flashMessage = request.session.flashMessage;
     request.session.flashMessage = "";
-    response.render("Jewels_list", { "Jewels": Jewels, "flashMessage": flashMessage });
+    response.render("jewel_list", { "Jewels": Jewels, "flashMessage": flashMessage });
 }
 async function JewelShowAction(request, response) {
     // response.send("SHOW ACTION");
-    var oneJewel = await JewelRepo.getOneJewel(request.params.Jewel_ID);
-    response.render("Jewel_show", { "oneJewel": oneJewel });
+    var OneJewel = await JewelRepo.getOneJewel(request.params.Jewel_ID);
+    response.render("Jewel_show", { "Jewel": OneJewel });
 }
 async function JewelEditAction(request, response) {
     // response.send("EDIT ACTION");
-    var AllJewels = await JewelRepo.getAllJewels();
-    var Jewel_ID = request.params.Jewel_ID;
-    if (Jewel_ID!=="0")
-        var jewels = await JewelRepo.getOneJewel(Jewel_ID);
+    var OneJewel = await JewelRepo.getOneJewel(request.params.Jewel_ID);
+    var Jewel_id = request.params.Jewel_ID;
+    if (Jewel_id!=="0")
+        var jewel = await JewelRepo.getOneJewel(Jewel_id);
     else
-        var jewels = JewelRepo.getBlankJewels();
-    response.render("Jewel_edit", { "Jewel": jewels, "Name": AllJewels });
+        var jewel = JewelRepo.getBlankJewels();
+    response.render("Jewel_edit", { "Jewel":jewel,"name":OneJewel });
 }
-/*
 async function JewelDelAction(request, response) {
     // response.send("DEL ACTION");
     // TODO: remove extras for car, unless the car cannot be removed!!!
-    var numRows = await JewelRepo.delOneCar(request.params.Jewel_ID);
+    var numRows = await JewelRepo.delOneJewel(request.params.Jewel_ID);
     request.session.flashMessage = "ROWS DELETED: "+numRows;
-    response.redirect("/cars/list");
-}*/ 
+    response.redirect("/Jewels/list");
+} 
 
 async function JewelUpdateAction(request, response) {
     // response.send("UPDATE ACTION");
@@ -50,7 +49,7 @@ async function JewelUpdateAction(request, response) {
     if (Jewel_ID==="0") Jewel_ID = await JewelRepo.addOneJewel(request.body.Jewel_name);
     // var isFancy = request.body.car_isFancy === undefined ? 0 : 1; 
     var numRows = await JewelRepo.editOneJewel(
-        Jewel_ID, 
+    	Jewel_ID, 
         request.body.Jewel_material, 
         request.body.size, 
         request.body.price, 
