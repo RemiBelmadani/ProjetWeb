fb9b6a7b3e8e408038418744367b6e7df3e7595a
steve_the_sheep <81408972+stve-the-sheep@users.noreply.github.com>
2022-12-09 13:58:06 +0100

    Update userRepository.js

--- userRepository.js
+++ userRepository.js
@@ -6,7 +6,7 @@ module.exports = {
       let conn = await pool.getConnection();
       let sql = "SELECT Users_ID, Users_name,Users_mail,Users_phone,Users_address,Users_role, FROM Users WHERE Users_ID = ?,"; // must leave out the password+hash
       const rows = await conn.query(sql, Users_name);
-      conn.end();
+      conn.release();
 
       if (rows.length == 1) {
         return rows[0];
@@ -19,15 +19,15 @@ module.exports = {
     }
   },
   
-  async areValidCredentials(username, password) {
+  async areValidCredentials(Users_name, password) {
     try {
       let conn = await pool.getConnection();
-      let sql = "SELECT * FROM Users WHERE Users_name = '?' AND Users_passwords COLLATE utf8mb4_general_ci  = sha2(concat(Users_created, ?), 224) COLLATE utf8mb4_general_ci"; 
+      let sql = "SELECT * FROM Users WHERE Users_name = ? AND Users_passwords COLLATE utf8mb4_general_ci  = sha2(concat(user_created, ?), 224) COLLATE utf8mb4_general_ci "; 
       // TODO: better salt+pw hash - COLLATE usually not needed
-      const rows = await conn.query(sql, [username, password]);
-      conn.end();
+      const rows = await conn.query(sql, [Users_name, password]);
+      conn.release();
 
-      if (rows.length == 1 && rows[0].Users_name === username) {
+      if (rows.length == 1 && rows[0].Users_name === Users_name) {
         return true;
       } else {
         return false;
